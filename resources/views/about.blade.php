61435e2ff282f52b1da57ed0cfa3c6109005d21e
AyuPutri31 <ayupuutri0gmail.com>
2022-10-24 08:16:47 +0700

    menyelesaikan video 4 laravel Blade Templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Ayu Blog | About</title>
-</head>
-
-<body>
+@section('container')
     <h1>Halaman About</h1>
-    <h3><?= $name ?></h3>
-    <p><?= $email ?></p>
-    <img src="img/<?= $image ?>" alt="<?= $name ?>" width="200">
-
-    <script src="js/script.js"></script>
-</body>
-
-</html>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $email }}" width="200">
+@endsection
